7eb2650d813adb05fccdaf1439f04b5d8a5e10c0
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-15 03:31:53 +0200

    Controlled searchbar input  and form submittal - prevent default behaviour on form submittal - TODO: invoke a callback in child SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = e => {
+        e.preventDefault();
+        console.log(e);
+    }
+
     render() {
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <div className="ui left icon input">
-                            <input type="text" placeholder="search for your favourite video..."></input>
+                            <input
+                                type="text"
+                                placeholder="search for your favourite video..."
+                                value={this.state.term}
+                                onChange={ e => this.setState({ term: e.target.value.toUpperCase() }) }>
+                            </input>
                             <i className="video icon"></i>
                         </div>
                     </div>
