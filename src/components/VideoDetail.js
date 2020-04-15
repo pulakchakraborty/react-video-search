5af9bea7b180deb1581ec8c7005a4e2767be36b3
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-16 01:08:17 +0200

    Add iframe tag for youtube videos

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,8 +7,13 @@ const VideoDetail = ({ video }) => {
         );
     }
 
+    const videoSrc = `https://youtube.com/embed/${video.id.videoId}`;
+
     return(
         <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
